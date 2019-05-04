db8998ce770ffa95c0287d913dd771c17b7bc110
moises eastland <dewaynepade95@gmail.com>
2019-05-03 22:21:08 -0500

    final update

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log ('this is laded');
+
+exports. spotify ={
+    id: process.env.SPOTTIFY_ID,
+    secert:process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const keys = require("./keys.js");
+
+const spotify= new spotify(keys.spotify);
+
+//i was unable to to create a proper .env file 
+
+//i was unbale to use the axio with my lack of understanding 
+
+//i didn't know where to start to use the ticketmaster API
+
+//i didn't know where to start to use the omdb API 
+
+// i didn't know where to start to use the spotify API
+
+// i don't have a full understanding on how console log with node 
+
+// i don't have a full understanding on how to call the APIs to the the terminal and get a return 
